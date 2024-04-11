a6dd21ad974aaa86b872843d7e98ff7f667d4c62
Sameer Humagain <sameer@orderup.com>
2024-04-11 14:08:05 +1000

    encryption update

--- main.py
+++ main.py
@@ -5,63 +5,65 @@ from logics import keygen, enc, get_primes_between
 
 
 def decrypt(ency, d, pk):
-    print("Decrypting Number:" + str(ency))
+    # print("Decrypting Number:" + str(ency))
     decrypted = enc(ency, d, pk)
-    print("After Decrypt:" + str(decrypted))
+    # print("After Decrypt:" + str(decrypted))
     return decrypted
 
 
 def encrypt(a, e, pk):
-    print("Encrypting Number:" + str(a))
+    # print("Encrypting Number:" + str(a))
     encrypted = enc(a, e, pk)
-    print("Result Encryption:" + str(encrypted))
+    # print("Result Encryption:" + str(encrypted))
     return encrypted
 
 
-def encrypt_all(data_sets, e, primary_key):
-    prs = get_primes_between(500, 1000, len(data_sets))
-    prsa = get_primes_between(1, 500, len(data_sets))
-    se = []
-    for k in data_sets:
-        ds = data_sets.index(k)
-        ex = encrypt(prs[ds], e, primary_key)
-        ey = encrypt(prsa[ds], e, primary_key)
-        se.append([ex, ey])
-    return se
+class EncryptDecrypt:
+    def __init__(self, encrypt_key, decrypt_key, shared_primary_key):
+        self._e = encrypt_key
+        self._d = decrypt_key
+        self._primary_key = shared_primary_key
+        self.enc_data = []
+        self.dec_data = []
+        existing_datasets = dataset()
+        dataset_primes = get_primes_between(500, 1000, len(existing_datasets))
+        self.dataset_values = list(zip(dataset_primes, existing_datasets))
+        self.dataset_values_inverse = list(zip(existing_datasets, dataset_primes))
 
+    def get_encrypted_data(self, input_data):
+        input_char_value = [char for char in input_data]
+        elements_value = []
+        for char in input_char_value:
+            for dt_char, val in self.dataset_values_inverse:
+                if char == dt_char:
+                    elements_value.append([char, val])
 
-def decrypt_all(enc_array, d, primary_key):
-    sd = []
-    for x, y in enc_array:
-        dx = decrypt(x, d, primary_key)
-        dy = decrypt(y, d, primary_key)
-        sd.append([dx, dy])
-    return sd
+        list_dataset_encrypted_array = [encrypt(value, self._e, self._primary_key) for (key, value) in elements_value]
+        return list_dataset_encrypted_array
+
+    def get_decrypted_data(self, encrypted_data):
+        list_dataset_decrypted_array = [decrypt(list_dataset_enc, self._d, self._primary_key) for list_dataset_enc in
+                                        encrypted_data]
+        decrypted_data = []
+        for decrypt_value in list_dataset_decrypted_array:
+            for char_value, char in self.dataset_values:
+                if char_value == decrypt_value:
+                    decrypted_data.append(char)
+
+        return decrypted_data
 
 
 if __name__ == '__main__':
-    # datasets = dataset()
-    e, d, primary_key = keygen()
-    # encrypted_array = encrypt_all(datasets, e, primary_key)
-    # decrypted_array = decrypt_all(encrypted_array, d, primary_key)
-    # print(decrypted_array)
-
+    # e, d, primary_key = keygen()
     print("+++++++++++++++++++++")
-
-    a = 53
-    enc_val = encrypt(a, e, primary_key)
-    decrypt(enc_val, d, primary_key)
-
+    e, primary_key = 17, 271271
+    encDecDataUsage = EncryptDecrypt(encrypt_key=e, decrypt_key=0, shared_primary_key=primary_key)
+    encrypted_array = encDecDataUsage.get_encrypted_data("HelloWorld")
+    print(encrypted_array)
     print("+++++++++++++++++++++")
-
-    b = 31
-    enc_val = encrypt(b, e, primary_key)
-    decrypt(enc_val, d, primary_key)
-
-    print("+++++++++++++++++++++")
-
-    c = 113
-    enc_val = encrypt(c, e, primary_key)
-    decrypt(enc_val, d, primary_key)
-
+    d, primary_key = 492353, 271271
+    decDecDataUsage = EncryptDecrypt(encrypt_key=0, decrypt_key=d, shared_primary_key=primary_key)
+    encrypted_array = [257211, 11958, 67645, 67645, 71233, 1644, 71233, 158594, 67645, 79792]
+    decrypted_array = decDecDataUsage.get_decrypted_data(encrypted_array)
+    print(decrypted_array)
     print("+++++++++++++++++++++")
